c463d2a2b6a6fce19966d46913ca298ced5d7db7
SwimmingPigLuvr <dnevell95@gmail.com>
2023-05-24 18:35:51 -0700

    firebase setup time

---
+++ src/components/Login.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+    import { userStore } from "sveltefire";
+
+    const user = userStore(auth);
+
+</script>
+
+<body>
+
+    <div class="fixed top-3 right-6">
+        {#if $user}
+            <p class="font-input tracking-tighter font-white text-xl px-6 py-3">Hello {$user.id}</p>
+        {:else}
+            <button class="hover:bg-black hover:bg-opacity-50 text-white text-xl px-6 py-3 font-input-compressed">Log in</button>
+        {/if}
+    </div>
+</body>
--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import Todo from "../../components/Todo.svelte";
-
-
+    import Login from "../../components/Login.svelte";
+    import { FirebaseApp } from "sveltefire";
 </script>
 
 <body>
+
+<Login />
 <Todo />
+
 </body>
 
 
